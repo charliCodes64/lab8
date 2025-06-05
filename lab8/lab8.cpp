45a3507af1995c3d3179507a7386d57b9785d04b
charliCodes64 <cinfinge@mail.umw.edu>
2025-06-05 01:25:49 -0400

    final revised

--- lab8/lab8.cpp
+++ lab8/lab8.cpp
@@ -16,16 +16,17 @@ int main()
     ALLEGRO_BITMAP* image = NULL;
     const float FPS = 60;
     const int width = 900;
-    const int height = 700;
-    const int ogreSize = 32;
+    const int height = 800;
+    const int ogreSize = 210;
     float ogreX = width / 2 - ogreSize / 2;
     float ogreY = height / 2 - ogreSize / 2;
-    float ogreDX = -4.0, ogreDY = 4.0;
+    float ogreDX = 0, ogreDY = 0;
     float angle = 0;
     bool redraw = true;
-    bool exit = false;
+    bool stop = false;
 
     al_init();
+    al_install_keyboard();
     al_init_image_addon();
     timer = al_create_timer(1.0 / FPS);
 
@@ -42,42 +43,77 @@ int main()
         al_destroy_display(display);
         al_destroy_timer(timer);
     }
+
     al_register_event_source(event_queue, al_get_display_event_source(display));
     al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
     al_clear_to_color(al_map_rgb(0, 0, 0));
     al_flip_display();
     al_start_timer(timer);
 
-    while (!exit) {
+    while (1) {
         ALLEGRO_EVENT ev;
         al_wait_for_event(event_queue, &ev);
 
-        if (ev.keyboard.keycode == ALLEGRO_KEY_LEFT) {
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-            if (ev.keyboard.keycode == ALLEGRO_KEY_LEFT) {
-                angle -= 0.1;
-                al_draw_rotated_bitmap(ogre, width, height, ogreX, ogreY, angle, 0);
-            }
-        }
-        if (ev.type == ALLEGRO_EVENT_TIMER) {
-             if (ogreX < 0 || ogreX > width - ogreSize) {
-                ogreDX = -ogreDX;
-            }
-            if (ogreY < 0 || ogreY > height - ogreSize) {
-                ogreDY = -ogreDY;
-            }
-            ogreX += ogreDX;
-            ogreY += ogreDY;
-
-            redraw = true;
-        }
-        else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+        if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE || ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
             break;
         }
+        if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+            if (ev.keyboard.keycode == ALLEGRO_KEY_LEFT) {
+                ogreDX = -7; //setting speed by direction
+                ogreDY = 0;
+                angle = 0;
+            }
+            if (ev.keyboard.keycode == ALLEGRO_KEY_RIGHT) {
+                ogreDX = 7;
+                ogreDY = 0;
+                angle = M_PI; //used to flip
+            }
+            if (ev.keyboard.keycode == ALLEGRO_KEY_UP) {
+                ogreDY = -7; 
+                ogreDX = 0;   
+                angle = M_PI / 2; 
+            }
+            if (ev.keyboard.keycode == ALLEGRO_KEY_DOWN) {
+                ogreDY = 7;
+                ogreDX = 0;
+                angle = -M_PI / 2;
+            }
+            if (ev.keyboard.keycode == ALLEGRO_KEY_SPACE) {
+                stop = !stop;
+            }
+        }
+        //same format from lab 8 video 
+        if (ev.type == ALLEGRO_EVENT_TIMER) {
+            if(!stop) {
+                if (ogreX < 0 || ogreX > width - ogreSize) {//handleing left and right
+                    ogreDX = -ogreDX;
+                    angle = M_PI;
+                    if (ogreDX < 0) {
+                        angle = 0;
+                    }
+                }
+
+                if (ogreY < 0 || ogreY > height - ogreSize) {//handleing up and down
+                    ogreDY = -ogreDY;
+                    angle = -M_PI / 2;
+                     if (ogreDY < 0) {
+                        angle = M_PI / 2;
+                    }
+                }
+                ogreX += ogreDX;
+                ogreY += ogreDY;
+            }
+            redraw = true;
+        }
+        //reused from lab 8
         if (redraw && al_is_event_queue_empty(event_queue)) {
             al_clear_to_color(al_map_rgb(0, 0, 0));
-            al_draw_bitmap(ogre, ogreX, ogreY, 0);
+            al_draw_scaled_bitmap(image, 0, 0, al_get_bitmap_width(image), al_get_bitmap_height(image), 0, 0, width, height,1);
+            //al_draw_bitmap(image, width, height, 0);
+            al_draw_rotated_bitmap(ogre, ogreSize / 2, ogreSize / 2, ogreX + ogreSize / 2, ogreY + ogreSize / 2, angle, 0);
             al_flip_display();
+            redraw = false;
         }
     }
     al_destroy_bitmap(ogre);
